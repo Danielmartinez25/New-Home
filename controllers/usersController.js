bd1119e5d6390989ce644667a56844d367260160
Danimartinez25 <danimartinez1325@gmail.com>
2022-09-28 13:20:28 -0300

    iniciando sequelize y archivos anteriores

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,85 +1,85 @@
-    const bcryptjs = require ('bcryptjs');
-const {validationResult} = require('express-validator');
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 /* User de models */
-const User = require ('../models/User');
+const User = require("../modelsUser/User");
 
 module.exports = {
+  register: (req, res) => {
+    return res.render("register", {
+      title: "Register",
+    });
+  },
 
-    register : (req,res) => {
-        return res.render('register',{
-            title: 'Register'
-        })
-    },
+  processRegister: (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      let userToCreate = {
+        ...req.body,
+        password: bcryptjs.hashSync(req.body.password, 10),
+        avatar: req.file.filename,
+      };
 
-    processRegister: (req,res) => {
-        const errors = validationResult(req);
-        if(errors.isEmpty()){
-        let userToCreate = {
-            ...req.body,
-            password: bcryptjs.hashSync (req.body.password, 10),
-            avatar : req.file.filename
-            
-        }
+      User.create(userToCreate);
 
-        User.create (userToCreate);
-
-        return res.redirect ('/');    
-        }
-        else{
-              return res.render("register", {
-                title: "Register",
-                errors: errors.mapped(),
-                old: req.body
-              });
-        }  
-    },
-
-    login : (req,res) => {
-        return res.render('login',{
-            title: 'Login'
-        })
-    },
-
-    loginProcess: (req,res) => {
-        let userToLogin = User.findByTag('email', req.body.email);
-
-        if (userToLogin) {
-            let isCorrectPassword = bcryptjs.compareSync (req.body.password , userToLogin.password);
-            if (isCorrectPassword){
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
-                return res.redirect ('/users/profile')
-            }
-            return res.render ('login', {
-                title: 'Login',
-                errors: {
-                    email: {
-                        msg : 'las creedenciales son invalidas'
-                    }
-                }
-            });
-        }
-
-        return res.render ('login', {
-            title: 'Login',
-            errors: {
-                email: {
-                    msg : 'Este email no se encuentra en nuestra base de datos'
-                }
-            }
-        });
-    },
-
-    profile: (req,res) => {
-        return res.render ('userProfile', {
-            title: 'Perfil',
-            user: req.session.userLogged
-        })
-    },
-    logout : (req,res) => {
-        req.session.destroy();
-        res.cookie('newHome',null,{maxAge: -1});
-        return res.redirect('/');
+      return res.redirect("/");
+    } else {
+      return res.render("register", {
+        title: "Register",
+        errors: errors.mapped(),
+        old: req.body,
+      });
     }
-}
+  },
+
+  login: (req, res) => {
+    return res.render("login", {
+      title: "Login",
+    });
+  },
+
+  loginProcess: (req, res) => {
+    let userToLogin = User.findByTag("email", req.body.email);
+
+    if (userToLogin) {
+      let isCorrectPassword = bcryptjs.compareSync(
+        req.body.password,
+        userToLogin.password
+      );
+      if (isCorrectPassword) {
+        delete userToLogin.password;
+        req.session.userLogged = userToLogin;
+        return res.redirect("/users/profile");
+      }
+      return res.render("login", {
+        title: "Login",
+        errors: {
+          email: {
+            msg: "las creedenciales son invalidas",
+          },
+        },
+      });
+    }
+
+    return res.render("login", {
+      title: "Login",
+      errors: {
+        email: {
+          msg: "Este email no se encuentra en nuestra base de datos",
+        },
+      },
+    });
+  },
+
+  profile: (req, res) => {
+    return res.render("userProfile", {
+      title: "Perfil",
+      user: req.session.userLogged,
+    });
+  },
+  logout: (req, res) => {
+    req.session.destroy();
+    res.cookie("newHome", null, { maxAge: -1 });
+    return res.redirect("/");
+  },
+};
--- routes/products.js
+++ routes/products.js
@@ -15,7 +15,7 @@ router
     .get('/edit/:id',adminUserCheck,edit)
     .put('/update/:id',validatorEditProduct,update)
     .post('/store',uploadImageProduct.single('image'),validatorAddProduct,store)
-    .get('/create',create) 
+    .get('/create', adminUserCheck,create) 
     .get('/cart',cart)  
     .get('/delete/:id',adminUserCheck, remove)   
     .delete('/destroy/:id',destroy)
