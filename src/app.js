cc2b0681952d6a0dbd2634f7941cf5326b2ec7bd
Danimartinez25 <danimartinez1325@gmail.com>
2022-10-06 14:39:02 -0300

    insertando en carpeta src

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ let productsRouter = require("./routes/products");
 let app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"));
@@ -25,7 +25,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(
   session({
     secret: "Shhh, It's a secret",
