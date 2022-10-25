d2329a897a40f3045b50b857ec48ffb04b6340ca
Danimartinez25 <danimartinez1325@gmail.com>
2022-10-25 15:06:07 -0300

    delete

--- src/Sprints/Sprint 3/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const { index } = require('../controllers/indexController');
-var router = express.Router();
-
-/* / */
-router.get('/', index);
-
-module.exports = router;
